892cc329dcd26c107b4ec7eb94f71aec1c8289b3
lucascampos04 <camposdlucasoli@gmail.com>
2024-03-20 19:08:32 -0300

    Commenting code

--- LED_Control_Challenge.cpp
+++ LED_Control_Challenge.cpp
@@ -13,27 +13,28 @@ void setup()
 
 void loop()
 {
-  pinValor = analogRead(A0);
-  Serial.println(pinValor);
+  pinValor = analogRead(A0); // Lê o valor analógico do pino A0
+  Serial.println(pinValor); // Imprime o valor lido no monitor serial
 
-  if (pinValor >= 500 && pinValor < 699){
-    digitalWrite(led, HIGH);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, LOW);
+  if (pinValor >= 500 && pinValor < 699) { // Se o valor está entre 500 e 699
+    digitalWrite(led, HIGH); // Acende o primeiro LED
+    digitalWrite(led2, LOW); // Apaga o segundo LED
+    digitalWrite(led3, LOW); // Apaga o terceiro LED
   }
-  else if (pinValor >= 700 && pinValor < 999){
-    digitalWrite(led, LOW);
-    digitalWrite(led2, HIGH);
-    digitalWrite(led3, LOW);
+  else if (pinValor >= 700 && pinValor < 999) { // Se o valor está entre 700 e 999
+    digitalWrite(led, LOW);  // Apaga o primeiro LED
+    digitalWrite(led2, HIGH); // Acende o segundo LED
+    digitalWrite(led3, LOW);  // Apaga o terceiro LED
   }
-  else if (pinValor >= 1000){
-    digitalWrite(led, LOW);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, HIGH);
+  else if (pinValor >= 1000) { // Se o valor é maior ou igual a 1000
+    digitalWrite(led, LOW);  // Apaga o primeiro LED
+    digitalWrite(led2, LOW);  // Apaga o segundo LED
+    digitalWrite(led3, HIGH); // Acende o terceiro LED
   }
-  else {
-    digitalWrite(led, LOW);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, LOW);
+  else { // Se o valor não se encaixa em nenhuma das condições anteriores
+    digitalWrite(led, LOW); // Apaga o primeiro LED
+    digitalWrite(led2, LOW); // Apaga o segundo LED
+    digitalWrite(led3, LOW); // Apaga o terceiro LED
   }
 }
+
