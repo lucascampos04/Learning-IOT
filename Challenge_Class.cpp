27a9e0c18cf7851d86e5374d50d8077da9a0efb1
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-16 19:24:25 -0300

    codigo atualizado

--- Challenge_Class.cpp
+++ Challenge_Class.cpp
@@ -18,8 +18,9 @@ int lastMessage = 0;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup() {
-    Serial.begin(9600);
+void setup()
+{
+    //Serial.begin(9600);
 
     // Inicialização e configuração do display LCD
     lcd.begin(16, 2);
@@ -35,107 +36,101 @@ void setup() {
 }
 
 // Função para exibir mensagem de alarme desligado no LCD
-void messageAlarmeDesligado() {
+void messageSensores(bool ligado)
+{
     // Limpa o LCD se a última mensagem não foi a de alarme desligado
-    if (lastMessage != 1)
-        lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Alarme desligado");
-    lastMessage = 1;
-}
-
-// Função para exibir o estado da porta no LCD
-void messagePorta(bool open) {
-    String message;
-
-    // Define a mensagem com base no estado da porta
-    if (open)
-        message = "Porta  aberta   ";
-    else
-        message = "Porta  fechada  ";
-
-    // Exibe a mensagem no LCD
-    lcd.setCursor(0, 1);
-    lcd.print(message);
-}
-
-// Função para exibir o estado da janela no LCD
-void messageJanela(bool open) {
-    String message;
-
-    // Define a mensagem com base no estado da janela
-    if (open)
-        message = "Janela aberta  ";
-    else
-        message = "Janela fechada ";
-
-    // Limpa o LCD se a última mensagem não foi a do estado da janela
-    if (lastMessage != 2)
+    if ((lastMessage != 1 && ligado) || (lastMessage != 2 && !ligado))
         lcd.clear();
 
-    // Exibe a mensagem no LCD
     lcd.setCursor(0, 0);
-    lcd.print(message);
-    lastMessage = 2;
-}
-
-// Função para exibir o estado do sensor de movimento no LCD
-void messageInfravermelho(bool detectado) {
-    // Limpa o LCD se a última mensagem não foi a do sensor de movimento
-    if ((lastMessage != 3 && detectado) || (lastMessage != 4 && !detectado))
-        lcd.clear();
-
-    // Exibe a mensagem no LCD de acordo com o estado do sensor de movimento
-    lcd.setCursor(0, 0);
-    lcd.print("Sensor Movimento");
-
-    if (detectado) {
-        lcd.setCursor(0, 1);
-        lcd.print("Detectado");
-        lastMessage = 3;
-    } else {
-        lcd.setCursor(0, 1);
-        lcd.print("Ativado");
-        lastMessage = 4;
+    lcd.print("Sensores");
+  	lcd.setCursor(0, 1);
+  	
+  	if(ligado)
+    {
+  		lcd.print("ativados");
+      	lastMessage = 1;
+      	return;
     }
+  
+	lcd.print("desativados");
+	lastMessage = 2;
+}
+
+void messageAlarme(int index)
+{
+  	// Limpa o LCD se a última mensagem não foi do mesmo sensor.
+    if (lastMessage != 3 + index)
+        lcd.clear();
+  
+  	// Exibe a mensagem no LCD de acordo com o estado do sensor.
+    lcd.setCursor(0, 0);
+    lcd.print("Alarme acionado");
+  	lcd.setCursor(0, 1);
+  	
+  	String sensores[] = {"Janela","Porta","Corredor"};
+  	lcd.print(sensores[index]);
+  
+  	lastMessage = 3 + index;
 }
 
 // Função para ativar o alarme
-void soarAlarme(bool ligado) {
-    digitalWrite(led, ligado);
-    digitalWrite(alarme, ligado);
+void soarAlarme(bool ligado)
+{
+  	digitalWrite(led, ligado);
+  	
+  	if(!ligado)
+    {
+      	noTone(alarme);
+      	return;
+    }
+	
+  	int tones[] = {440, 494, 523};
+	for(int i = 0; i < 3; i++)
+	{
+		float time = 100;
+		tone(alarme, tones[i], time);
+		Serial.println(tones[i]);
+      	delay(time);
+	} 	
+  
 }
 
-void loop() {
+void loop()
+{
     // Tocar os alarmes.
     soarAlarme(tocarAlarme);
 
     // Verificar se está de dia.
     bool dia = analogRead(A0) < 500;
 
-    if (dia) {
-        tocarAlarme = false;
-        // Mostrar informações no LCD.
-        messageAlarmeDesligado();
+    if (dia)
+    {
+      	messageSensores(false);
         return;
     }
 
     // Verificar se a porta e janela estão abertas.
-    janelaAberta = digitalRead(btnJanela) != 0;
-    portaAberta = digitalRead(btnPorta) != 0;
+    janelaAberta = digitalRead(btnJanela) == 0;
+    portaAberta = digitalRead(btnPorta) == 0;
 
-    if (!janelaAberta || !portaAberta) {
-        // Mostrar informações no LCD.
-        messageJanela(janelaAberta);
-        messagePorta(portaAberta);
-        tocarAlarme = false;
-        return;
-    }
+    if (janelaAberta)
+        messageAlarme(0);
+  
+  	if (portaAberta)
+     	messageAlarme(1);
 
     // Detectar movimento usando o PIR.
     movimentoDetectado = analogRead(A1) > 1;
 
-    // Exibir o estado do sensor de movimento no LCD e controlar o alarme
-    messageInfravermelho(movimentoDetectado);
-    tocarAlarme = movimentoDetectado;
+    if(movimentoDetectado)
+    	messageAlarme(2);
+  
+    // Tocar o alarme.
+  	tocarAlarme = movimentoDetectado || portaAberta || janelaAberta;
+  	
+  	if(tocarAlarme)
+      	return;
+  
+  	messageSensores(true);
 }
